314cc99408661016dd5e26c45ed8577b05a8a3de
Artem Khraban <rtm_shnd@Artems-MacBook-Pro.local>
2021-03-11 23:22:35 +0200

    commit

---
+++ 2layersPerceptron/main.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import pickle
+import numpy as np
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+(train_x, train_y), (test_x, test_y) = keras.datasets.fashion_mnist.load_data()
+models_folder = os.curdir + "/models/"
+
+labelNames = ["top", "trouser", "pullover", "dress", "coat",
+              "sandal", "shirt", "sneaker", "bag", "ankle boot"]
+
+INPUT_SHAPE = 784
+NUM_CATEGORIES = 10
+
+
+def fit_model(train_x_, train_y_, test_x_, test_y_, model_):
+    model_.fit(train_x_,
+               train_y_,
+               epochs=8,
+               batch_size=32,
+               validation_data=(test_x_, test_y_))
+    pickle.dump(model_, open(f'{models_folder}model.sav', 'wb'))
+
+
+def plot_value_array( predictions_array, true_label):
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+if __name__ == '__main__':
+
+    train_x = train_x.reshape(60000, 28 * 28)
+    test_x_normalized = test_x.reshape(10000, 28 * 28)
+    train_x = train_x.astype('float32') / 255
+    test_x = test_x.astype('float32') / 255
+
+    train_y = keras.utils.to_categorical(train_y)
+
+    # model = Sequential()
+    #
+    # model.add(Dense(512, input_dim=INPUT_SHAPE))
+    # model.add(Activation('relu'))
+    #
+    # model.add(Dense(NUM_CATEGORIES))
+    # model.add(Activation('softmax'))
+    #
+    # model.compile(optimizer='rmsprop',
+    #               loss='categorical_crossentropy',
+    #               metrics=['accuracy'])
+
+    # fit_model(train_x, train_y, test_x, test_y, model)
+
+    model = pickle.load(open(f'{models_folder}model.sav', 'rb'))
+
+    prediction = model.predict(test_x_normalized)
+    print(prediction[0])
+    labels = np.argmax(prediction, 1)
+    for x, y, z in list(zip(test_x, test_y, labels)):
+        plt.title(f"Predicted: {labelNames[z]}({100 * np.max(prediction)}); From table: {labelNames[y]}")
+        plt.imshow(x)
+        plt.show()
+        cmd = str(input()).lower()
+        if cmd == "q":
+            break
